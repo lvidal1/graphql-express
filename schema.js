cbe0d56c42941e5f70f852959be2404ac007a391
lvidal1 <lvidal910@gmail.com>
2017-08-01 01:00:30 -0500

    Added json-server & axios to get remote data

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -7,13 +8,13 @@ const {
     GraphQLNonNull
 } = require ('graphql');
 
-// Hardcoded data
-const customers = [
-    {id : '1', name: 'John Doe', email: "joh@hod.com",age:25},
-    {id : '2', name: 'Sara Doe', email: "sara@hod.com",age:24},
-    {id : '3', name: 'William Doe', email: "will@hod.com",age:15},
-    {id : '4', name: 'Steven Doe', email: "steven@hod.com",age:20},
-]
+// // Hardcoded data
+// const customers = [
+//     {id : '1', name: 'John Doe', email: "joh@hod.com",age:25},
+//     {id : '2', name: 'Sara Doe', email: "sara@hod.com",age:24},
+//     {id : '3', name: 'William Doe', email: "will@hod.com",age:15},
+//     {id : '4', name: 'Steven Doe', email: "steven@hod.com",age:20},
+// ]
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -23,7 +24,9 @@ const CustomerType = new GraphQLObjectType({
             id: {type : GraphQLString },
             name: {type: GraphQLString},
             email: {type: GraphQLString},
-            age: {type: GraphQLInt}
+            phone: {type: GraphQLString},
+            age: {type: GraphQLInt},
+            website: {type: GraphQLString},
         }
     )
 })
@@ -40,15 +43,23 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parentValue,args){
                 // Return the customer by id
-                return customers.filter((c)=>{
-                    return c.id = args.id
-                })[0];
+                // Local data
+                // return customers.filter((c)=>{
+                //     return c.id = args.id
+                // })[0];
+                // Remote data
+                return axios.get('http://localhost:3000/customers/'+args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue,args){
-                return customers;
+                // Local data
+                // return customers;
+                // Remote data
+                return axios.get('http://localhost:3000/customers/')
+                    .then(res => res.data);
             }
         }
     }
